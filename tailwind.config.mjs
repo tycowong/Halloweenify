f8c6fc80217ba6c60a974f79bad821996796c08f
Roberto Wong <33732733+tycowong@users.noreply.github.com>
2024-10-10 11:08:47 -0700

    Playing with caludianry transformations

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+import { transformSlots } from "astro/jsx-runtime";
+import { transform } from "typescript";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        fadeDrop: "fadeIn 1s ease-in-out",
+      },
+
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0, transform: "translateY(-10px)" },
+          to: { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
