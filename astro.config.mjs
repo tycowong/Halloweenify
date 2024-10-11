8e4d636c3b8fab6d30636a32ab56443f3086cc7e
Roberto Wong <33732733+tycowong@users.noreply.github.com>
2024-10-11 15:40:58 -0700

    vercel folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 import fulldev from "fulldev-ui/integration";
+
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercelServerless(),
   integrations: [
     tailwind(),
     fulldev({
