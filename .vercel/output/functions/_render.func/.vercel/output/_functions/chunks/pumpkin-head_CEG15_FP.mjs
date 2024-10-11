1e15c476be859e820d78c8442036c9dfa630ed55
Roberto Wong <33732733+tycowong@users.noreply.github.com>
2024-10-11 15:15:08 -0700

    vercel issue

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/pumpkin-head_CEG15_FP.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./pumpkin-head_scwnnzs0.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const collectedScripts = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
