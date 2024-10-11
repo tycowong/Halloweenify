95f47b8932772eb42893224ced80926ad8be7a1b
Roberto Wong <33732733+tycowong@users.noreply.github.com>
2024-10-11 15:56:42 -0700

    vercel folder redo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
-import fulldev from "fulldev-ui/integration";
-
-import vercelServerless from "@astrojs/vercel/serverless";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercelServerless(),
-  integrations: [
-    tailwind(),
-    fulldev({
-      colors: {
-        theme: "dark",
-        dark: {
-          background: "#302D1D",
-          base: "#A0406A",
-          brand: "#E89523",
-        },
-        light: {
-          background: "#302D1D",
-          base: "#A0406A",
-          brand: "#E89523",
-        },
-      },
-    }),
-  ],
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,3 @@
-import { transformSlots } from "astro/jsx-runtime";
-import { transform } from "typescript";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
